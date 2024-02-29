a0e32dd6dc52782fb66f0244ca130de544c114a5
Abdellahsyani <syaniabdos@gmail.com>
2024-02-29 18:54:59 +0100

    root node

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -15,7 +15,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (ptr == NULL ||node == NULL)
+	if (ptr != NULL ||node != NULL)
 		return (0);
 	else
 		return (1);
