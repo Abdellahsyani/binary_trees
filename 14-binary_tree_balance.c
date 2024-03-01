452328823f20ba5f53429d91b401a8529793ec78
Abdellahsyani <syaniabdos@gmail.com>
2024-03-01 22:06:33 +0100

    balanced tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -33,8 +33,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height) ? left_height : right_height);
+	return (1 + (left_height > right_height) ? left_height : right_height);
 }
