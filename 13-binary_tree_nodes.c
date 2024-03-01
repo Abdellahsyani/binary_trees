25e1e7822d4f742b60667c458737cd837aea2c16
Abdellahsyani <syaniabdos@gmail.com>
2024-03-01 18:42:00 +0100

    count nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,10 @@
 #include "binary_trees.h"
 
-/***/
+/**
+ * binary_tree_nodes - count nodes
+ * @tree: the root of tree
+ * Return: the sum of nodes that have one child
+ */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
@@ -8,31 +12,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
-	count += (tree->left || tree->right ? 1: 0);
+
+	count += (tree->left || tree->right ? 1 : 0);
 	count += binary_tree_nodes(tree->left);
 	count += binary_tree_nodes(tree->right);
 
 	return (count);
-}
-
-int main(void)
-{
-	binary_tree_t *root;
-	size_t nodes;
-
-	root = binary_tree_node(NULL, 98);
-	root->left = binary_tree_node(root, 12);
-	root->right = binary_tree_node(root, 402);
-	binary_tree_insert_right(root->left, 54);
-	binary_tree_insert_right(root, 128);
-	binary_tree_print(root);
-
-	nodes = binary_tree_nodes(root);
-	printf("Nodes in %d: %lu\n", root->n, nodes);
-	nodes = binary_tree_nodes(root->right);
-	printf("Nodes in %d: %lu\n", root->right->n, nodes);
-	nodes = binary_tree_nodes(root->left->right);
-	printf("Nodes in %d: %lu\n", root->left->right->n, nodes);
-	return (0);
 }
