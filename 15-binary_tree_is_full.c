c06a0d03030fe1f030676d1be54fbb0267bfb7ab
Abdellahsyani <syaniabdos@gmail.com>
2024-03-01 22:33:20 +0100

    check full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,15 +8,14 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (1);
+	if (!tree)
+		return (0);
 
-		if (tree->left != NULL && tree->right != NULL)
-		{
-			return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-		}
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	}
-	return (0);
 }
