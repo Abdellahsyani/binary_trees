1fca732f88c90e38fe1c28ee519e560ccb5e565d
Abdellahsyani <syaniabdos@gmail.com>
2024-03-01 16:19:57 +0100

    height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,8 +11,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	size_t left_height = 0, right_height = 0;
+
+	binary_tree_height(tree->left);
+	binary_tree_height(tree->right);
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
