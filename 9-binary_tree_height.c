50e52052b146d03ee66e80308bad17f262ebef9f
Abdellahsyani <syaniabdos@gmail.com>
2024-03-01 16:35:48 +0100

    claculate height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0, right_height = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = 0, right_height = 0;
 
-	binary_tree_height(tree->left);
-	binary_tree_height(tree->right);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	return ((left_height > right_height) ? left_height : right_height);
 }
