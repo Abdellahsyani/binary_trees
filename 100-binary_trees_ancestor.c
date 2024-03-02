dee93bdac7ecdf6fec179dbc9a5d66b4d43f8061
Abdellahsyani <syaniabdos@gmail.com>
2024-03-02 12:18:03 +0100

    ancestors

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - function to return common ancestor
+ * @first: the first node
+ * @second: the second node
+ * Return: NULL if fail and nodes if success
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	const binary_tree_t *first_ances = first;
+	const binary_tree_t *second_ances = second;
+
+	while (first_ances != NULL)
+	{
+		second_ances = second;
+		while (second_ances != NULL)
+		{
+			if (first_ances == second_ances)
+				return ((binary_tree_t *)first_ances);
+			second_ances = second_ances->parent;
+		}
+		first_ances = first_ances->parent;
+	}
+	return (NULL);
+}
